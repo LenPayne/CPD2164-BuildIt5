4bc2ab372852a362f57329716feea34459b50b73
Len Payne <len.payne@lambtoncollege.ca>
2014-11-17 14:03:32 -0500

    Initial Commit

---
+++ CPD2164-BuildIt5/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  CPD2164-BuildIt5
+//
+//  Created by Len Payne on 2014-11-17.
+//  Copyright (c) 2014 Len Payne. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
